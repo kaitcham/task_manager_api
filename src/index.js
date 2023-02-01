5a0f6fb5de0fc99bb390598682fbfc0bacaa11c2
Munezero Christian <chamsf5@aol.com>
2023-02-01 13:20:40 +0200

    Custom error handler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./db/connect');
 const tasks = require('./routes/tasks');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 
 const port = 3000;
 const app = express();
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/tasks', tasks);
+
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
